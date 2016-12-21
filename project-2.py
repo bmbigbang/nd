67bab0df0fa87cea51726fc29ca617b685039b38
Ardavan Shafiee <bm.bigbang@gmail.com>
2016-12-21 06:35:09 +0000

    initial project 2 code with previous code from deep learning course

---
+++ project-2.py
@@ -0,0 +1,157 @@
+# Load pickled data
+import pickle
+
+# TODO: Fill this in based on where you saved the training and testing data
+
+training_file = 'training.pickle'
+testing_file = 'test.pickle'
+
+with open(training_file, mode='rb') as f:
+    train = pickle.load(f)
+with open(testing_file, mode='rb') as f:
+    test = pickle.load(f)
+
+X_train, y_train = train['features'], train['labels']
+X_test, y_test = test['features'], test['labels']
+
+### Replace each question mark with the appropriate value.
+
+# TODO: Number of training examples
+n_train = len(train['features'])
+
+# TODO: Number of testing examples.
+n_test = len(test['features'])
+
+# TODO: What's the shape of an traffic sign image?
+image_shape = train['features'][0].shape
+
+# TODO: How many unique classes/labels there are in the dataset.
+n_classes = len(set([i[1] for i in train['labels'] + test['labels']]))
+
+print("Number of training examples =", n_train)
+print("Number of testing examples =", n_test)
+print("Image data shape =", image_shape)
+print("Number of classes =", n_classes)
+
+### Data exploration visualization goes here.
+### Feel free to use as many code cells as needed.
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+# Visualizations will be shown in the notebook.
+
+plt.imshow(dataset[22], interpolation='nearest', cmap=cm.brg)
+plt.show()
+# %matplotlib inline
+
+
+### Step 1. Preprocess the data here.
+### Feel free to use as many code cells as needed.
+
+### Q1. Generate data additional data (OPTIONAL!)
+### and split the data into training/validation/testing sets here.
+### Feel free to use as many code cells as needed.
+
+### Q2. Define your architecture here.
+### Feel free to use as many code cells as needed.
+
+import tensortflow as tf
+import numpy as np
+
+def accuracy(predictions, labels):
+  return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+          / predictions.shape[0])
+batch_size = 16
+patch_size = 5
+depth = 16
+num_hidden = 64
+
+graph = tf.Graph()
+
+with graph.as_default():
+    # Input data.
+    tf_train_dataset = tf.placeholder(
+    tf.float32, shape=(batch_size, image_size, image_size, num_channels))
+    tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+    tf_valid_dataset = tf.constant(valid_dataset)
+    tf_test_dataset = tf.constant(test_dataset)
+
+    # Variables.
+    layer1_weights = tf.Variable(tf.truncated_normal(
+        [patch_size, patch_size, num_channels, depth], stddev=0.1))
+    layer1_biases = tf.Variable(tf.zeros([depth]))
+    layer2_weights = tf.Variable(tf.truncated_normal(
+        [patch_size, patch_size, depth, depth], stddev=0.1))
+    layer2_biases = tf.Variable(tf.constant(1.0, shape=[depth]))
+    layer3_weights = tf.Variable(tf.truncated_normal(
+        [image_size // 4 * image_size // 4 * depth, num_hidden], stddev=0.1))
+    layer3_biases = tf.Variable(tf.constant(1.0, shape=[num_hidden]))
+    layer4_weights = tf.Variable(tf.truncated_normal(
+        [num_hidden, num_labels], stddev=0.1))
+    layer4_biases = tf.Variable(tf.constant(1.0, shape=[num_labels]))
+
+    #tf.nn.max_pool()
+    # Model.
+    def model(data):
+        conv = tf.nn.conv2d(data, filter=layer1_weights, strides=[1, 1, 1, 1], padding='SAME')
+        conv = tf.nn.max_pool(conv, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+        hidden = tf.nn.relu(conv + layer1_biases)
+        conv = tf.nn.conv2d(hidden, filter=layer2_weights, strides=[1, 1, 1, 1], padding='SAME')
+        conv = tf.nn.max_pool(conv, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+        hidden = tf.nn.relu(conv + layer2_biases)
+        shape = hidden.get_shape().as_list()
+        reshape = tf.reshape(hidden, [shape[0], shape[1] * shape[2] * shape[3]])
+        hidden = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
+        return tf.matmul(hidden, layer4_weights) + layer4_biases
+
+
+    # dropout layer
+    keep_prob = tf.Variable(0.5)
+    hidden_layer_drop = tf.nn.dropout(model(tf_train_dataset), keep_prob)
+
+    # Training computation.
+    logits = hidden_layer_drop
+    loss = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
+
+    # introducing variable learning rate
+    global_step = tf.Variable(0)  # count the number of steps taken.
+    learning_rate = tf.train.exponential_decay(0.1, global_step, 150, 0.9)
+    # Optimizer.
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+
+    # Predictions for the training, validation, and test data.
+    train_prediction = tf.nn.softmax(logits)
+    valid_prediction = tf.nn.softmax(model(tf_valid_dataset))
+    test_prediction = tf.nn.softmax(model(tf_test_dataset))
+
+### Q3. Train your model here.
+### Feel free to use as many code cells as needed.
+    num_steps = 3001
+
+    with tf.Session(graph=graph) as session:
+        tf.initialize_all_variables().run()
+        print('Initialized')
+        for step in range(num_steps):
+            offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+            batch_data = train_dataset[offset:(offset + batch_size), :, :, :]
+            batch_labels = train_labels[offset:(offset + batch_size), :]
+            feed_dict = {tf_train_dataset: batch_data, tf_train_labels: batch_labels}
+            _, l, predictions = session.run(
+                [optimizer, loss, train_prediction], feed_dict=feed_dict)
+            if (step % 50 == 0):
+                print('Minibatch loss at step %d: %f' % (step, l))
+                print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
+                print('Validation accuracy: %.1f%%' % accuracy(valid_prediction.eval(), valid_labels))
+        print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
+
+### Step 2. Load the images and plot them here.
+### Feel free to use as many code cells as needed.
+
+
+### Q6. Run the predictions here.
+### Feel free to use as many code cells as needed.
+
+### Q7. Visualize the softmax probabilities here.
+### Feel free to use as many code cells as needed.
+
+### Q8. Use the model's softmax probabilities to visualize the certainty of its predictions

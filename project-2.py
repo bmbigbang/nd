f21bd6b1eb46e5612620f03b8a92f099fc9ccf06
bmbigbang <kLEpmB3j0msuZ2I8>
2016-12-23 19:31:16 +0000

    LeNet implementation integrated with numerous bugfixes to normalization and greyscale dimensions

--- project-2.py
+++ project-2.py
@@ -24,7 +24,7 @@ n_test = len(test['features'])
 
 # TODO: What's the shape of an traffic sign image?
 image_shape = train['features'][0].shape
-num_channels = 1 # grey scale
+num_channels = 1  # grey scale
 
 # TODO: How many unique classes/labels there are in the dataset.
 n_classes = max(train['labels']) - min(train['labels']) + 1
@@ -66,23 +66,51 @@ ax[1].table(cellText=a[1], colLabels=['Sign', 'Count'], loc='center', fontsize=1
 ax[2].table(cellText=a[2], colLabels=['Sign', 'Count'], loc='center', fontsize=13).auto_set_font_size(False)
 # plt.plot(b[0], b[1], label='Count of labels vs label id')
 
-plt.show()
+# plt.show()
 
 
 ### Step 1. Preprocess the data here.
 ### Feel free to use as many code cells as needed.
 import tensorflow as tf
-# reformat to grayscale
-X_train = X_train.reshape((-1, image_shape[0], image_shape[0], num_channels)).astype(np.float32)
-X_test = X_test.reshape((-1, image_shape[0], image_shape[0], num_channels)).astype(np.float32)
+from sklearn.utils import shuffle
 # create validation set
 X_valid, X_test = np.split(X_test, 2)
 y_valid, y_test = np.split(y_test, 2)
-# normalize
-X_train = tf.nn.l2_normalize(X_train, [0, 0, 0])
-X_test = tf.nn.l2_normalize(X_test, [0, 0, 0])
-X_valid = tf.nn.l2_normalize(X_valid, [0, 0, 0])
+# randomize training/test data
+X_train, y_train = shuffle(X_train, y_train)
+X_test, y_test = shuffle(X_test, y_test)
+# reformat to grayscale
+temp = np.zeros_like(X_train, dtype=np.float32)
+temp = temp.reshape((-1, image_shape[0], image_shape[0], num_channels))
+for i, j in enumerate(X_train):
+    for k, l in enumerate(j):
+        temp[i][k] = np.array([np.mean(l)])
+temp = temp[:i + 1,:,:]
+X_train = temp
 
+temp = np.zeros_like(X_valid, dtype=np.float32)
+temp = temp.reshape((-1, image_shape[0], image_shape[0], num_channels))
+for i, j in enumerate(X_valid):
+    for k, l in enumerate(j):
+        temp[i][k] = np.array([np.mean(l)])
+temp = temp[:i + 1,:,:]
+X_valid = temp
+
+temp = np.zeros_like(X_test, dtype=np.float32)
+temp = temp.reshape((-1, image_shape[0], image_shape[0], num_channels))
+for i, j in enumerate(X_test):
+    for k, l in enumerate(j):
+        temp[i][k] = np.array([np.mean(l)])
+temp = temp[:i + 1,:,:]
+X_test = temp
+
+
+# normalize training data
+X_train = ((X_train - X_train.min()) * (X_train.min() / X_train.max())) / (X_train.max() - X_train.min())
+
+X_test = ((X_test - X_test.min()) * (X_test.min() / X_test.max())) / (X_test.max() - X_test.min())
+
+X_valid = ((X_valid - X_valid.min()) * (X_valid.min() / X_valid.max())) / (X_valid.max() - X_valid.min())
 
 ### Q1. Generate data additional data (OPTIONAL!)
 ### and split the data into training/validation/testing sets here.
@@ -91,93 +119,105 @@ X_valid = tf.nn.l2_normalize(X_valid, [0, 0, 0])
 ### Q2. Define your architecture here.
 ### Feel free to use as many code cells as needed.
 
-
+from tensorflow.contrib.layers import flatten
 def accuracy(predictions, labels):
   return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
           / predictions.shape[0])
 batch_size = 128
 filter_size = 5
-depth = 16
-num_hidden = 64
+depth = 6
+depth2 = 16
 
-graph = tf.Graph()
 
-with graph.as_default():
-    # Input data.
-    tf_train_dataset = tf.placeholder(
-        tf.float32, shape=(batch_size, image_shape[0], image_shape[1], num_channels))
-    tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, n_classes))
-    tf_valid_dataset = tf.constant(X_valid)
-    tf_test_dataset = tf.constant(X_test)
-
-    # Variables.
-    layer1_weights = tf.Variable(tf.truncated_normal(
-        [filter_size, filter_size, num_channels, depth], stddev=0.1))
-    layer1_biases = tf.Variable(tf.zeros([depth]))
-    layer2_weights = tf.Variable(tf.truncated_normal(
-        [patch_size, patch_size, depth, depth], stddev=0.1))
-    layer2_biases = tf.Variable(tf.constant(1.0, shape=[depth]))
-    layer3_weights = tf.Variable(tf.truncated_normal(
-        [image_size // 4 * image_size // 4 * depth, num_hidden], stddev=0.1))
-    layer3_biases = tf.Variable(tf.constant(1.0, shape=[num_hidden]))
-    layer4_weights = tf.Variable(tf.truncated_normal(
-        [num_hidden, num_labels], stddev=0.1))
-    layer4_biases = tf.Variable(tf.constant(1.0, shape=[num_labels]))
-
-    #tf.nn.max_pool()
-    # Model.
-    def model(data):
-        conv = tf.nn.conv2d(data, filter=layer1_weights, strides=[1, 1, 1, 1], padding='SAME')
-        conv = tf.nn.max_pool(conv, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-        hidden = tf.nn.relu(conv + layer1_biases)
-        conv = tf.nn.conv2d(hidden, filter=layer2_weights, strides=[1, 1, 1, 1], padding='SAME')
-        conv = tf.nn.max_pool(conv, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-        hidden = tf.nn.relu(conv + layer2_biases)
-        shape = hidden.get_shape().as_list()
-        reshape = tf.reshape(hidden, [shape[0], shape[1] * shape[2] * shape[3]])
-        hidden = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
-        return tf.matmul(hidden, layer4_weights) + layer4_biases
-
+def model(data):
+    conv1 = tf.nn.conv2d(data, filter=layer1_weights, strides=[1, 1, 1, 1], padding='VALID')
+    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    conv1 = tf.nn.relu(conv1 + layer1_biases)
+    conv2 = tf.nn.conv2d(conv1, filter=layer2_weights, strides=[1, 1, 1, 1], padding='VALID')
+    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    conv2 = tf.nn.relu(conv2 + layer2_biases)
+    fc0 = flatten(conv2)
+    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean=0, stddev=0.1))
+    fc1_b = tf.Variable(tf.zeros(120))
+    fc1 = tf.nn.relu(tf.matmul(fc0, fc1_W) + fc1_b)
+    fc2_W = tf.Variable(tf.truncated_normal(shape=(120, 84), mean=0, stddev=0.1))
+    fc2_b = tf.Variable(tf.zeros(84))
+    fc2 = tf.nn.relu(tf.matmul(fc1, fc2_W) + fc2_b)
+    fc3_W = tf.Variable(tf.truncated_normal(shape=(84, n_classes),  mean=0, stddev=0.1))
+    fc3_b = tf.Variable(tf.zeros(n_classes))
 
     # dropout layer
     keep_prob = tf.Variable(0.5)
-    hidden_layer_drop = tf.nn.dropout(model(tf_train_dataset), keep_prob)
+    return tf.nn.dropout(tf.matmul(fc2, fc3_W) + fc3_b, keep_prob)
 
-    # Training computation.
-    logits = hidden_layer_drop
-    loss = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
 
-    # introducing variable learning rate
-    global_step = tf.Variable(0)  # count the number of steps taken.
-    learning_rate = tf.train.exponential_decay(0.1, global_step, 150, 0.9)
-    # Optimizer.
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
 
-    # Predictions for the training, validation, and test data.
-    train_prediction = tf.nn.softmax(logits)
-    valid_prediction = tf.nn.softmax(model(tf_valid_dataset))
-    test_prediction = tf.nn.softmax(model(tf_test_dataset))
+# Variables and Input data.
+tf_train_dataset = tf.placeholder(tf.float32, shape=(None, image_shape[0], image_shape[1], num_channels))
+tf_train_labels = tf.placeholder(tf.int32, shape=(None))
+layer1_weights = tf.Variable(tf.truncated_normal(
+    [filter_size, filter_size, num_channels, depth], mean=0, stddev=0.1))
+layer1_biases = tf.Variable(tf.zeros(depth))
+layer2_weights = tf.Variable(tf.truncated_normal(
+    [filter_size, filter_size, depth, depth2], mean=0, stddev=0.1))
+layer2_biases = tf.Variable(tf.zeros(depth2))
+# Training computation.
+one_hot_y = tf.one_hot(tf_train_labels, n_classes)
+logits = model(tf_train_dataset)
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, one_hot_y))
+
+# introducing variable learning rate
+global_step = tf.Variable(0)  # count the number of steps taken.
+learning_rate = tf.train.exponential_decay(0.002, global_step, 15000, 0.96)
+# Optimizer.
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+training_operation = optimizer.minimize(loss, global_step=global_step)
+# Predictions for the training, validation, and test data.
+# train_prediction = tf.nn.softmax(logits)
+# valid_prediction = tf.nn.softmax(model(tf_valid_dataset))
+# test_prediction = tf.nn.softmax(model(tf_test_dataset))
 
 ### Q3. Train your model here.
 ### Feel free to use as many code cells as needed.
-num_steps = 3001
+EPOCHS = 10
 
-with tf.Session(graph=graph) as session:
-    tf.initialize_all_variables().run()
-    print('Initialized')
-    for step in range(num_steps):
-        offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
-        batch_data = train_dataset[offset:(offset + batch_size), :, :, :]
-        batch_labels = train_labels[offset:(offset + batch_size), :]
-        feed_dict = {tf_train_dataset: batch_data, tf_train_labels: batch_labels}
-        _, l, predictions = session.run(
-            [optimizer, loss, train_prediction], feed_dict=feed_dict)
-        if (step % 50 == 0):
-            print('Minibatch loss at step %d: %f' % (step, l))
-            print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
-            print('Validation accuracy: %.1f%%' % accuracy(valid_prediction.eval(), valid_labels))
-    print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, batch_size):
+        batch_x, batch_y = X_data[offset:offset+batch_size], y_data[offset:offset+batch_size]
+        accuracy = sess.run(accuracy_operation, feed_dict={tf_train_dataset: batch_x, tf_train_labels: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+
+    print("Training...")
+    print()
+    for i in range(EPOCHS):
+        for offset in range(0, num_examples, batch_size):
+            end = offset + batch_size
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training_operation, feed_dict={tf_train_dataset: batch_x, tf_train_labels: batch_y})
+
+        validation_accuracy = evaluate(X_valid, y_valid)
+        print("EPOCH {} ...".format(i+1))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+        test_accuracy = evaluate(X_test, y_test)
+        print("Test Accuracy = {:.3f}".format(test_accuracy))
+
+# try:
+#     saver
+# except NameError:
+#     saver = tf.train.Saver()
+# saver.save(sess, 'lenet')
+# print("Model saved")
 
 ### Step 2. Load the images and plot them here.
 ### Feel free to use as many code cells as needed.
